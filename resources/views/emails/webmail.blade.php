d1e87307772ff449a0172b8a6501faaa6ef6a5d7
Riyan Satria <riyan.satria.619@icloud.com>
2026-04-02 02:41:16 +0700

    Role added to webmail access

--- resources/views/emails/webmail.blade.php
+++ resources/views/emails/webmail.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h2 style="margin-top:0;">{{ $subject }}</h2>
+<h2 style="margin-top:0;font-size: 20px;font-weight: 700;">{{ $subject }}</h2>
 
 @foreach ($bodies as $body)
     <p>{{ $body }}</p>
